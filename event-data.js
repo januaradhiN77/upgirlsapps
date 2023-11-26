5c8f52014fc8dbab9a09129cdf947436ed7b43d7
adhinugroho623@gmail.com <adhinugroho623@gmail.com>
2023-11-26 18:41:39 +0700

    h

---
+++ event-data.js
@@ -0,0 +1,86 @@
+var urlParams = new URLSearchParams(window.location.search);
+var eventId = urlParams.get('event');
+
+if (eventId === 'minishow24nov23') {
+    document.getElementById('judulShow').innerHTML = 'Minishow Up Girls Carousel';
+    document.getElementById('tanggalShow').innerHTML = 'Jumat, 24 November • 15:30';
+    document.getElementById('locationShow').innerHTML = 'Bandung Creative Hub';
+    document.getElementById('imageShow').src = 'imgeventban/upgirlsevent.jpg';
+    document.getElementById('alamatLengkap').innerHTML = 'Jl. Laswi No.7 Kacapiring, Bandung, Jawa Barat';
+//DATA MEMBER PERFORM
+
+document.getElementById('mem1').innerHTML = 'Una';
+
+
+document.getElementById('mem2').innerHTML = 'Rin';
+
+
+document.getElementById('mem3').innerHTML = 'Rea';
+
+
+document.getElementById('mem4').innerHTML = 'Ute';
+
+
+document.getElementById('mem5').innerHTML = 'Fia';
+
+document.getElementById('mem6').innerHTML = 'Vey';
+
+document.getElementById('mem7').innerHTML = 'Nat';
+
+document.getElementById('mem8').innerHTML = 'Ura';
+
+document.getElementById('mem9').innerHTML = 'Dee';
+
+document.getElementById('mem10').innerHTML = 'Liz';
+const applyLink = document.getElementById("applyLink");
+
+applyLink.addEventListener("click", () => {
+    window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLScgJB8Cj-e-xCXUFQxii_4IdznjvFoHvN_rbcutH_ZAtPRfOA/viewform'; // Use the URL directly
+});
+
+    // Menyesuaikan href tombol
+    
+} 
+
+
+
+if (eventId === 'minishow29okt23') {
+    document.getElementById('judulShow').innerHTML = 'The Upgirls Presents Mini Sho';
+    document.getElementById('tanggalShow').innerHTML = 'Minggu, 29 Oktober • 16:00';
+    document.getElementById('locationShow').innerHTML = 'C7HOMECOFFEE';
+    document.getElementById('imageShow').src = 'imgeventban/upgirlsevent.jpg';
+    document.getElementById('alamatLengkap').innerHTML = '-';
+//DATA MEMBER PERFORM
+
+document.getElementById('mem1').innerHTML = 'Una';
+
+
+document.getElementById('mem2').innerHTML = 'Rin';
+
+
+document.getElementById('mem3').innerHTML = 'Rea';
+
+
+document.getElementById('mem4').innerHTML = 'Ute';
+
+
+document.getElementById('mem5').innerHTML = 'Fia';
+
+document.getElementById('mem6').innerHTML = 'Vey';
+
+document.getElementById('mem7').innerHTML = 'Nat';
+
+document.getElementById('mem8').innerHTML = 'Ura';
+
+document.getElementById('mem9').innerHTML = 'Dee';
+
+document.getElementById('mem10').innerHTML = 'Liz';
+const applyLink = document.getElementById("applyLink");
+
+applyLink.addEventListener("click", () => {
+    window.location.href = 'https://forms.gle/8DtPMENKn3J5XFB88'; // Use the URL directly
+});
+
+    // Menyesuaikan href tombol
+    
+} 
